858e47fce8c0ba50bf684073d5570cccf4639692
scalajeremy <scalajeremy@gmail.com>
2019-03-20 16:40:34 +0100

    Updates

---
+++ precache-manifest.a01340d51322e2538c6f20d6f3dd2212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e23b8454cb102a513354f883fe33f9",
+    "url": "/Happy-Drink/static/media/splash2.69e23b84.svg"
+  },
+  {
+    "revision": "19c8b3571ebdd90566f4f66f6384d670",
+    "url": "/Happy-Drink/static/media/logo.19c8b357.svg"
+  },
+  {
+    "revision": "9b5288e8258e8d7e7c23",
+    "url": "/Happy-Drink/static/js/runtime~main.ec3119bf.js"
+  },
+  {
+    "revision": "7515e12eebdff0c8727a",
+    "url": "/Happy-Drink/static/js/main.5a4524aa.chunk.js"
+  },
+  {
+    "revision": "e49443d93783623d793d",
+    "url": "/Happy-Drink/static/js/2.34802895.chunk.js"
+  },
+  {
+    "revision": "7515e12eebdff0c8727a",
+    "url": "/Happy-Drink/static/css/main.bee28776.chunk.css"
+  },
+  {
+    "revision": "6ba36eb16561ebc826e7897f3303167c",
+    "url": "/Happy-Drink/index.html"
+  }
+];
